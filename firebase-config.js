701a25dd011edd53228d9cb9fa3cc7c86b26151f
Reeshimevcha <159103614+Reeshimevcha@users.noreply.github.com>
2026-03-16 09:07:10 +0530

    Update Firebase configuration settings
    Signed-off-by: Reeshimevcha <159103614+Reeshimevcha@users.noreply.github.com>

--- firebase-config.js
+++ firebase-config.js
@@ -1,12 +1,10 @@
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
+// For Firebase JS SDK v7.20.0 and later, measurementId is optiona 
+  const FIREBASE_CONFIG = {
   apiKey: "AIzaSyBph8A8njpDLIwE9JfB_Yr7j9CdKvKqDQA",
   authDomain: "voidchat-72ed4.firebaseapp.com",
+  databaseURL: "https://voidchat-72ed4-default-rtdb.asia-southeast1.firebasedatabase.app",
   projectId: "voidchat-72ed4",
-  storageBucket: "voidchat-72ed4.firebasestorage.app",
+  storageBucket: "voidchat-72ed4.appspot.com",
   messagingSenderId: "355987303332",
-  appId: "1:355987303332:web:a516052ee354afa174ef31",
-  measurementId: "G-7EFMT3C0K0",
-  databaseURL: "https://voidchat-72ed4-default-rtdb.asia-southeast1.firebasedatabase.app"
-
+  appId: "1:355987303332:web:a516052ee354afa174ef31"
 };
