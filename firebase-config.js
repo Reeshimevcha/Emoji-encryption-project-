eb089c284fce86e1c16117781532c9565921a73c
RISHIMEVCHA <159103614+Reeshimevcha@users.noreply.github.com>
2026-04-09 21:11:51 +0530

    Change Firebase API key in configuration
    Updated Firebase API key for configuration.
    
    Signed-off-by: RISHIMEVCHA <159103614+Reeshimevcha@users.noreply.github.com>

--- firebase-config.js
+++ firebase-config.js
@@ -1,6 +1,6 @@
 // Firebase Configuration — VoidChat
 const firebaseConfig = {
-  apiKey: "AIzaSyBph8A8njpDLIwE9JfB_Yr7j9CdKvKqDQA",
+  apiKey: "AIzaSyAMfnx1Ia87oJwRXQfuvCtBnZxWOi8ZbQ4",
   authDomain: "voidchat-72ed4.firebaseapp.com",
   databaseURL: "https://voidchat-72ed4-default-rtdb.asia-southeast1.firebasedatabase.app",
   projectId: "voidchat-72ed4",
